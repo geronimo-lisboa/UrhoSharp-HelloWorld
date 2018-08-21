55b009a6b5b831650d57a5806845e7fa76c7682f
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-08-20 23:48:17 -0300

    Carreguei com sucesso meu objeto.

--- GameTest/GameTest/MyGame.cs
+++ GameTest/GameTest/MyGame.cs
@@ -9,7 +9,8 @@ namespace GameTest
 {
     public class MyGame : Application
     {
-        Node cameraNode;
+        Node CameraNode;
+        Camera camera;
         Node earthNode;
         Node rootNode;
         Scene scene;
@@ -30,15 +31,57 @@ namespace GameTest
 
         protected override async void Start()
         {
-            HelloWorld();
+            Log.LogMessage += e => Debug.WriteLine($"[{e.Level}] {e.Message}");
+            base.Start();
+            //HelloWorld("Teste cubo");
+            UseMyCube();
         }
-        
-        async void HelloWorld()
+
+        async void UseMyCube()
+        {
+            var cache = this.ResourceCache;
+            //cria a cena
+            scene = new Scene(this.Context);
+            scene.CreateComponent<Octree>();
+            ////cria o node do plano
+            //var planeNode = scene.CreateChild("Plane");
+            //planeNode.Scale = new Vector3(100, 1, 100);
+            //var planeObject = planeNode.CreateComponent<StaticModel>();
+            //planeObject.Model = cache.GetModel("Models/Plane.mdl");
+            //planeObject.SetMaterial(cache.GetMaterial("Materials/Material.003.xml"));
+
+
+            //cria o node do cubo
+            var cubeNode = scene.CreateChild("Cube");
+            cubeNode.Position = new Vector3(0, 1, 10);
+            var cubeObject = cubeNode.CreateComponent<StaticModel>();
+            cubeObject.Model = cache.GetModel("Models/MyCube.mdl");
+            cubeObject.SetMaterial(cache.GetMaterial("Materials/Material _25.xml"));
+
+            //cria o node da luz
+            var lightNode = scene.CreateChild("DirectionalLight");
+            lightNode.SetDirection(new Vector3(0.6f, -1.0f, 0.8f)); // The direction vector does not need to be normalized
+            var light = lightNode.CreateComponent<Light>();
+            light.LightType = LightType.Directional;
+            //cria o node da câmera
+            CameraNode = scene.CreateChild ("camera");
+			camera = CameraNode.CreateComponent<Camera>();
+			CameraNode.Position = new Vector3(0, 5, -100);
+            //Com isso eu tenho uma luz, uma camera e um objeto qqer pendurados na cena
+
+            // Viewport
+            var viewport = new Viewport(Context, scene, camera, null);
+            viewport.SetClearColor(Color.Yellow);
+            Renderer.SetViewport(0, viewport);
+
+        }
+
+        async void HelloWorld(string msg)
         {
             //Criação do texto - o texto é adicionado à UI
             var helloText = new Text()
             {
-                Value = "Hello World from MySample",
+                Value = msg,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center
             };
